2f69e4191f61cf6ea49ef41770bd68cc096ab41a
CurseARealSword <cursearealsword@local>
2025-06-06 14:12:00 +0200

    working around psysqlite3

--- app4.py
+++ app4.py
@@ -1,8 +1,8 @@
 # imports
-__import__('pysqlite3')
+# __import__('pysqlite3')
 import sys
 import random
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 from dotenv import load_dotenv
